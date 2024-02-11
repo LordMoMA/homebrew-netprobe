f24cea55a4b719e7b883d435c61e46de9dd3b146
LordMoMA <lihongyi0512@gmail.com>
2024-02-11 17:32:25 -0500

    “newsha256”

---
+++ README.md
@@ -0,0 +1,1 @@
+Change url and sha256 accordingly
--- netprobe.rb
+++ netprobe.rb
@@ -1,8 +1,8 @@
 class Netprobe < Formula
   desc "Your software description"
   homepage "https://github.com/LordMoMA/NetProbe"
-  url "https://github.com/LordMoMA/NetProbe/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "0af2e668f3be38ddaaa176f4edf4eec86b50ba4cc20dc711e27e1d41e3ced0f5"
+  url "https://github.com/LordMoMA/NetProbe/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "684d74b8fa0dd39b0ae7cb816daf8a17ec2258711f34f7f8c8969c9e55ecec30"
 
   depends_on "go" => :build
 
