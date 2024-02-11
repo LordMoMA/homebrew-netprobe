2567e79d0ebdf7ccaad7347027f2b25984b1b82f
LordMoMA <lihongyi0512@gmail.com>
2024-02-11 15:55:03 -0500

    “first”

--- netprobe.rb
+++ netprobe.rb
@@ -2,7 +2,7 @@ class Netprobe < Formula
     desc "A CLI tool for network interface statistics"
     homepage "https://github.com/LordMoMA/NetProbe"
     url "https://github.com/LordMoMA/NetProbe/archive/v1.0.0.tar.gz"
-    sha256 "80649dacc9eaf61626b5e0abc6a08d0c86658426bab9853dccdd0cd88c17f471"
+    sha256 "81f8639ce8ada021a6d32e78bc603da5d1c00da3ad7be3420cea468ff805b23d"
   
     def install
       bin.install "netprobe"
