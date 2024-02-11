6bf1c736ddff9f7f11660819b418fc83698ab799
LordMoMA <lihongyi0512@gmail.com>
2024-02-11 17:13:13 -0500

    “newsha256”

--- netprobe.rb
+++ netprobe.rb
@@ -2,7 +2,7 @@ class Netprobe < Formula
   desc "Your software description"
   homepage "https://github.com/LordMoMA/NetProbe"
   url "https://github.com/LordMoMA/NetProbe/archive/v1.0.0.tar.gz"
-  sha256 "c64ad277213353d73f5e3cab4c003d5182f1c747366ecda6258106dfa8b60f89"
+  sha256 "0af2e668f3be38ddaaa176f4edf4eec86b50ba4cc20dc711e27e1d41e3ced0f5"
 
   depends_on "go" => :build
 
