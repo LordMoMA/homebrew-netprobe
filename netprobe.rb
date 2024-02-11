1cdea520c133108288fcd1fcf9ce97cdf65a83c5
LordMoMA <lihongyi0512@gmail.com>
2024-02-11 15:59:18 -0500

    “first”

--- netprobe.rb
+++ netprobe.rb
@@ -1,10 +1,17 @@
 class Netprobe < Formula
-    desc "A CLI tool for network interface statistics"
-    homepage "https://github.com/LordMoMA/NetProbe"
-    url "https://github.com/LordMoMA/NetProbe/archive/v1.0.0.tar.gz"
-    sha256 "81f8639ce8ada021a6d32e78bc603da5d1c00da3ad7be3420cea468ff805b23d"
-  
-    def install
-      bin.install "netprobe"
-    end
+  desc "Your software description"
+  homepage "https://github.com/LordMoMA/NetProbe"
+  url "https://github.com/LordMoMA/NetProbe/archive/v1.0.0.tar.gz"
+  sha256 "81f8639ce8ada021a6d32e78bc603da5d1c00da3ad7be3420cea468ff805b23d"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "netprobe", "main.go"
+    bin.install "netprobe"
   end
+
+  test do
+    system "#{bin}/netprobe", "--version"
+  end
+end
